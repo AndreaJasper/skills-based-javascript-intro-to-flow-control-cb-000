c4a1591e145b42062b31addabd2cc91416968a37
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-10-01 21:46:36 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -27,14 +27,19 @@ function ageChecker(age) {
 
 function ternaryTeenager(age) {
   if(age >= 13 && age <= 19) {
-    return "You are a teenager"
+    return "You are a teenager";
   }
-  else {
-    "You are not a teenager"
+  else (age < 13 && age > 19){
+    "You are not a teenager";
   }
 }
 
 function switchAge(age) {
-
+ if (age >= 13 && age <= 19) {
+   return "You are a teenager";
+ } else {
+   (age < 13 && age > 19){
+     return "You have an age";
+   }
+ }
 }
- 
